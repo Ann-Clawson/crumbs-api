368b0b33f2dc8e15ea4d419821630926e0eebfd6
Ann Clawson <annmasonclawson@gmail.com>
2024-04-08 14:43:28 -0700

    creates cooks create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/customers" => "customers#create"
   post "/orders" => "orders#create"
+  post "/cooks" => "cooks#create"
 end
