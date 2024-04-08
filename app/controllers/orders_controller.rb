554617a11eba09973cfafd9192d9b30f63214fbb
Ann Clawson <annmasonclawson@gmail.com>
2024-04-08 14:29:57 -0700

    creates create action in order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrdersController < ApplicationController
+  def create
+    @order = Order.new(
+      customer_id: params[:customer_id],
+      total: params[:total],
+      payment_type: params[:payment_type],
+      delivery_status: params[:delivery_status],
+    )
+    if @order.save
+      render json: { message: "Order created successfully" }, status: :created
+    else
+      render json: { errors: @order.errors.full_messages }, status: :bad_request
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/customers" => "customers#create"
-  post "/cookies" => "cookies#create"
+  post "/orders" => "orders#create"
 end
