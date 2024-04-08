d0aecc0cc5740588ba81220ba687d952f4cb1101
Ann Clawson <annmasonclawson@gmail.com>
2024-04-08 14:45:20 -0700

    creates carted cooks create route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   post "/customers" => "customers#create"
   post "/orders" => "orders#create"
   post "/cooks" => "cooks#create"
+  post "/carted_cooks" => "carted_cooks#create"
 end
